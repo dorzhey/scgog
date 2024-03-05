44316e96eb48dec5d55a99c20d5eb8b92e19bf47
dorzhey <dorzhey@mail.ru>
2024-03-05 16:07:42 -0500

    made the code more readable, maintainable, and consistent with Python coding standards

--- scgog/data_preprocessing.py
+++ scgog/data_preprocessing.py
@@ -3,18 +3,46 @@ import muon as mu
 import scanpy as sc
 
 
-def preprocess_omics_data(file_path):
+def preprocess_omics_data(file_path: str) -> mu.MuData:
+    """
+    Preprocess omics data by reading from an .h5 file and making variable names unique.
+
+    Parameters:
+    - file_path (str): Path to the .h5 file.
+
+    Returns:
+    - mu.MuData: Preprocessed MuData object.
+    """
     mdata = mu.read_10x_h5(file_path)
     mdata.var_names_make_unique()
     
     return mdata
 
-def read_ann_data(file_path):
+
+def read_ann_data(file_path: str) -> pd.DataFrame:
+    """
+    Read annotation data from a .tsv.gz file.
+
+    Parameters:
+    - file_path (str): Path to the .tsv.gz file.
+
+    Returns:
+    - pd.DataFrame: Annotation data.
+    """
     ann_data = pd.read_csv(file_path, sep='\t', compression='gzip')
     return ann_data
 
-# QUALITY CONTROL
-def quality_control(mdata):
+
+def quality_control(mdata: mu.MuData) -> mu.MuData:
+    """
+    Apply quality control measures to multimodal (RNA and ATAC) data.
+
+    Parameters:
+    - mdata (mu.MuData): MuData object containing RNA and ATAC data.
+
+    Returns:
+    - mu.MuData: MuData object after applying quality control.
+    """
     # Compute QC metrics for RNA
     mdata['rna'].var['mt'] = mdata['rna'].var_names.str.startswith('MT-')
     sc.pp.calculate_qc_metrics(mdata['rna'], qc_vars=['mt'], percent_top=None, log1p=False, inplace=True)
@@ -25,22 +53,23 @@ def quality_control(mdata):
     mu.pp.filter_obs(mdata['rna'], 'n_genes_by_counts', lambda x: (x >= 200) & (x < 5000))
     mu.pp.filter_obs(mdata['rna'], 'total_counts', lambda x: x < 15000)
     mu.pp.filter_obs(mdata['rna'], 'pct_counts_mt', lambda x: x < 20)
-    
     # Filter ATAC based on quality metrics
     mu.pp.filter_obs(mdata['atac'], 'n_genes_by_counts', lambda x: (x >= 2000) & (x <= 15000))
     mu.pp.filter_obs(mdata['atac'], 'total_counts', lambda x: (x >= 4000) & (x <= 40000))
-    
     # Intersect observations to keep only cells present in both modalities
     mu.pp.intersect_obs(mdata)
     
     return mdata
 
-def merge_data(mdata):
+
+def merge_data(mdata: mu.MuData) -> mu.MuData:
     """
-    Merges scRNA-seq and scATAC-seq data into a single MuData object.
-    :param adata_rna: An AnnData object containing the scRNA-seq data.
-    :param adata_atac: An AnnData object containing the scATAC-seq data.
-    :return: A MuData object containing the merged datasets.
+    Merge scRNA-seq and scATAC-seq data into a single MuData object.
+
+    Parameters:
+    - mdata (mu.MuData): MuData object containing RNA and ATAC data.
+
+    Returns:
+    - mu.MuData: A MuData object containing the merged datasets.
     """
     return mu.MuData({'rna': mdata['rna'], 'atac': mdata['atac']})
-
